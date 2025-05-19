2b19cf5b4e572a377558a76a595957b1618d8e21
Stephen Smith <stephen.w.smith4@icloud.com>
2025-05-19 12:59:57 -0400

    rustlings complete

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -41,7 +41,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split(',');
+        let (Some(name), Some(age), None) = (split.next(), split.next(), split.next()) else {
+            return Err(ParsePersonError::BadLen);
+        };
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        let Ok(age) = age.parse::<u8>() else {
+            let err = age.parse::<u8>().unwrap_err();
+            return Err(ParsePersonError::ParseInt(err));
+        };
+        Ok(Person{
+            name: name.to_string(),
+            age,
+        })
+    }
 }
 
 fn main() {
--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -28,14 +28,36 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
 
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        match tuple {
+            (x, y, z) if (0..256).contains(&x) && (0..256).contains(&y) && (0..256).contains(&z) => {
+                Ok(Color {
+                    red:  x as u8,
+                    green: y as u8,
+                    blue: z as u8,
+                })
+            },
+            _ => Err(IntoColorError::IntConversion),
+        }
+    }
 }
 
 // TODO: Array implementation.
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        match (arr[0],arr[1],arr[2]) {
+            (x, y, z) if (0..256).contains(&x) && (0..256).contains(&y) && (0..256).contains(&z) => {
+                Ok(Color {
+                    red:  x as u8,
+                    green: y as u8,
+                    blue: z as u8,
+                })
+            },
+            _ => Err(IntoColorError::IntConversion),
+        }
+    }
 }
 
 // TODO: Slice implementation.
@@ -43,7 +65,21 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        match (slice[0], slice[1], slice[2]) {
+            (x, y, z) if (0..256).contains(&x) && (0..256).contains(&y) && (0..256).contains(&z) => {
+                Ok(Color {
+                    red:  x as u8,
+                    green: y as u8,
+                    blue: z as u8,
+                })
+            },
+            _ => Err(IntoColorError::IntConversion),
+        }
+    }
 }
 
 fn main() {
